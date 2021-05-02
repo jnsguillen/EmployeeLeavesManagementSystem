383a7aa51839b747fc74f69e0b377a6002aafb1c
jnsguillen <81977267+jnsguillen@users.noreply.github.com>
2021-05-02 18:48:54 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,58 @@
 ﻿using System;
 
-namespace judiya
+namespace myproject
 {
     class Program
     {
         static void Main(string[] args)
         {
- 		Console.WriteLine("EMPLOYEE LEAVES MANAGEMENT SYSTEM");
+        Console.WriteLine("EMPLOYEE LEAVES MANAGEMENT SYSTEM");
+		Login();  
+		 }
+		 
+	static void Login()
+	{
 		
+		int count = 0;
+		while (count < 5)
+		{
+			if (count == 4)
+			{
+				Console.WriteLine("Maximum tries exceeded!");
+			return;
+			}
+			
+			
+		//email: "employee@leaves.company";
+		//password: "password";
+		
+		Console.WriteLine("Enter email for Login: ");
+		string InputEmail= Console.ReadLine();
+		Console.WriteLine("Enter Password: ");
+		string InputPassword= Console.ReadLine();
+		
+		if (InputEmail=="employee@leaves.company"&& InputPassword == "password") 
+		{
+			Console.WriteLine("Welcome User!");
+			system();
+		}
+		
+		else {
+			count++;
+		}
+	}	
+}
+	
+	static void system()
+	{
+		Start1:
 		Console.WriteLine("type \"form\" to access request form for leave\ntype \"list\"  to access list of employees' absences");
-		string answer = Console.ReadLine();
+		string answer1 = Console.ReadLine();
 		
-		if (answer == "form"){
+		if (answer1 == "form"){
+			
+			var requestslist = new List<string>();
+				
 			Console.WriteLine("Employee name (Surname, First Name, Middle Initial): ");
 			string eName = Console.ReadLine();
 			
@@ -20,92 +61,65 @@ namespace judiya
 			
 			Console.WriteLine("Request given to: ");
 			string dReq = Console.ReadLine();
-			
+		
 			Console.WriteLine("Date of Request: ");
 			string date = Console.ReadLine();
 			
 			Console.WriteLine("Employee Name: "+eName+"\nType of leave Requesting: "+tLeave+"\nRequest given to: "+dReq+"\nDate of Request: "+date+"\nRequest submitted! Please wait for 3-5 business days for permission.");
+			
+			requestslist.Add(eName);
+			foreach (var employeenames in requestslist) 
+			{
+				Console.WriteLine("Leave requests: "+employeenames);
+			}
+			
+			goto Start1;
 		}
-		else if (answer == "list" ) 
-		{
-			Console.WriteLine("Enter an employee's name from the list to access their record: ");
-			
-			
-			Console.WriteLine("Fernandez, Jerrica E.\nGuillen,Judea Nicole S.\nLaluyan, Aliyah Mae Q.\nLiwanag, Hannah Arceli A.\nNavarro, Elton John S.\nOrcega, Jhyzel D.\nTecson, Erika P.");
+		
+		else if (answer1 =="list") {
+			Console.WriteLine("Guillen,Judea Nicole S.\nNavarro, Elton John S.");
 			string name2 = Console.ReadLine();
 			
-			{
-				if (name2.Equals("Fernandez, Jerrica E.", StringComparison.InvariantCultureIgnoreCase)) 
-				{
-					Fernandez();
-				}
-				else if (name2.Equals("Guillen, Judea Nicole S.", StringComparison.InvariantCultureIgnoreCase))
+			var recordslist = new List<string>()
+			{"Guillen, Judea Nicole S.", "Navarro, Elton John S."};
+			
+						foreach(var item in recordslist){
+						Console.WriteLine(item);
+						}
+			
+			if (name2.Equals("guillen, judea nicole s.", StringComparison.InvariantCultureIgnoreCase)) 
 				{
 					Guillen();
 				}
-				else if (name2.Equals("Laluyan, Aliyah Mae Q.", StringComparison.InvariantCultureIgnoreCase))
-				{
-					Laluyan();
-				}
-				else if (name2.Equals("Liwanag, Hannah Arceli A.", StringComparison.InvariantCultureIgnoreCase))
-				{
-					Liwanag();
-				}
-				else if(name2.Equals("Navarro, Elton John S.", StringComparison.InvariantCultureIgnoreCase))
-				{
-					Navarro();
-				}
-				else if (name2.Equals("Orcega, Jhyzel D.", StringComparison.InvariantCultureIgnoreCase))
-				{
-					Orcega();
-				}
+			else if (name2.Equals("guillen, judea nicole s.", StringComparison.InvariantCultureIgnoreCase))
+			{
+				Navarro();
 			}
-					}
 			
+			goto Start1;
+		}
 	}
+	
+		static void Guillen()
+		{
 			
-	static void Fernandez()
-	{
-		Console.WriteLine("PROFILE:\nFull name: Fernandez, Jerrica E.\nAge: 25\nAddress:Biñan, Laguna");
-		
-		int jerusedvac = 3; int jeravvac = 13;
-		int jersickus = 4; int jersickav = 24;
-		int jerunus = 0; int jerunav = 365;
-		int jerworus = 5; int jerworav = 25;
-		
-		Console.WriteLine("RECORD OF LEAVES:\nVACATION used:" +jerusedvac+ " available: "+jeravvac+"\nSICK LEAVE used:"+jersickus+ " available: "+jersickav+ "\nUNPAID LEAVE used: "+jerunus+" available: "+jerunav+"\nWORK FROM HOME used: "+jerworus+" available: "+jerworav);
-		
-	}
+			Console.WriteLine("PROFILE:\nFull name: Guillen, Judea Nicole S.\nAge: 24\nAddress:San Pedro, Laguna");
+			Console.WriteLine("Record of Leaves:" );
+			Console.WriteLine("Vacation: \n used:4 available: 12");
+			Console.WriteLine("SICK LEAVE used:2 available:26");
+			Console.WriteLine("UNPAID LEAVE used:0 available: 365");
+			Console.WriteLine("WORK FROM HOME used:5 available 25");
+		}
 	
-	static void Guillen()
-	{
-		Console.WriteLine("PROFILE:\nFull name: Guillen, Judea Nicole S.\nAge: 23\nAddress:San Pedro, Laguna");
-		Console.WriteLine("RECORD OF LEAVES:\nVACATION used:4 available: 12\nSICK LEAVE used:2 available:26\nUNPAID LEAVE used:0 available: 365\nWORK FROM HOME used:5 available 25");
-	}
-	
-	static void Laluyan()
-	{
-		Console.WriteLine("PROFILE:\nFull name: Laluyan, Aliyah Mae Q.\nAge: 24\nAddress:San Pedro, Laguna");
-		Console.WriteLine("RECORD OF LEAVES:\nVACATION used:1 available: 15\nSICK LEAVE used:0 available:28\nUNPAID LEAVE used:0 available: 365\nWORK FROM HOME used:6 available 24");
-	}
-	
-	static void Liwanag()
-	{
-		Console.WriteLine("PROFILE:\nFull name: Liwanag, Hannah Arceli A.\nAge: 29\nAddress:Biñan, Laguna");
-		Console.WriteLine("RECORD OF LEAVES:\nVACATION used:5 available: 11\nSICK LEAVE used:4 available:24\nUNPAID LEAVE used:3 available: 362\nWORK FROM HOME used:3 available 27");
-	}
-		
 	static void Navarro()
 	{
-		Console.WriteLine("PROFILE:\nFull name: Navarro, Elton John S.\nAge: 24\nAddress:Muntinlupa, Metro Manila");
-		Console.WriteLine("RECORD OF LEAVES:\nVACATION used:1 available: 15\nSICK LEAVE used:2 available:26\nUNPAID LEAVE used:0 available: 365\nWORK FROM HOME used:1 available 29");
+		Console.WriteLine("PROFILE:\nFull name: Navarro, Elton John S.\nAge: 25\nAddress:Tunasan, Muntinlupa");
+			Console.WriteLine("Record of Leaves:" );
+			Console.WriteLine("Vacation: \n used:5 available: 11");
+			Console.WriteLine("SICK LEAVE used:1 available:27");
+			Console.WriteLine("UNPAID LEAVE used:0 available: 365");
+			Console.WriteLine("WORK FROM HOME used:4 available 26");
 	}
 	
-	static void Orcega()
-	{
-		Console.WriteLine("PROFILE:\nFull name: Orcega, Jhyzel D.\nAge: 22\nAddress: Biñan, Laguna");
-		Console.WriteLine("RECORD OF LEAVES:\nVACATION used:6 available: 10\nSICK LEAVE used:5 available:23\nUNPAID LEAVE used:0 available: 365\nWORK FROM HOME used:1 available 29");
-	}
-	
-	}
 }
+
